b8b26b450d4c4b1a5b74aaab16768aaa410118c6
Ionut Costica <ionut.costica@gmail.com>
2016-02-20 21:01:09 +0200

    feat(build): extract gulp settings

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,83 +3,7 @@
 /**
  * Usage: node ./node_modules/gulp/bin/gulp.js ......
  */
-
-/**
- * Class to hold some recurring values used further in the script
- */
-class Settings {
-	constructor() {
-		this.appName = 'Udacity Event Planner';
-
-		this.docsFolder = './docs';
-		this.readmeFile = './README.md';
-
-		this.sourceApp = './src';
-		this.dist = './dist';
-
-		this.tsOutputPath = `${this.dist}/js`;
-		this.allTypeScript = `${this.sourceApp}/**/*.ts`;
-		this.allTypings = [
-			'./typings/typescriptApp.d.ts',
-			'./typings/browser.d.ts'
-		];
-		this.typingsOutputPath = './typings/typescriptApp.d.ts';
-
-		this.allAssets = [
-			`${this.sourceApp}/**/*`,
-			`!${this.allTypeScript}`
-		];
-		this.watchFiles = [
-			'/index.html',
-			'/**/*.js',
-			'/**/*.css',
-			'/**/*.png', '/**/*.jpg', '/**/*.jpeg', '/**/*.gif'
-		].map(file => this.dist + file);
-
-		this.indexHtml = `${this.sourceApp}/index.html`;
-		this.indexHtmlOut = `${this.dist}/index.html`;
-		this.libOutputPath = `${this.tsOutputPath}/lib`;
-		this.jsLibs = [
-			'./node_modules/es6-shim/es6-shim.js',
-			'./node_modules/angular2/bundles/angular2-polyfills.js',
-			'./node_modules/systemjs/dist/system.src.js',
-			'./node_modules/rxjs/bundles/Rx.js',
-			'./node_modules/angular2/bundles/angular2.dev.js',
-			'./node_modules/angular2/bundles/router.dev.js',
-			'./node_modules/angular2/bundles/http.dev.js'
-		];
-
-		this.testBase = './tests';
-		this.testFiles = [
-			`${this.testBase}/**/*.ts`
-		];
-		this.testFilesOut = `${this.testBase}/compiled`;
-		this.testFilesOutGlob = `${this.testFilesOut}/**/*.js`;
-		this.testMainPre = `${this.testBase}/unit-tests.pre.html`;
-		this.testMain = `${this.testBase}/unit-tests.html`;
-		this.testLibs = [
-			'./node_modules/jasmine-core/lib/jasmine-core/jasmine.js',
-			'./node_modules/jasmine-core/lib/jasmine-core/jasmine-html.js',
-			'./node_modules/jasmine-core/lib/jasmine-core/boot.js',
-			'./node_modules/es6-shim/es6-shim.js',
-			'./node_modules/angular2/bundles/angular2-polyfills.js',
-			'./node_modules/systemjs/dist/system.src.js',
-			'./node_modules/rxjs/bundles/Rx.js',
-			'./node_modules/angular2/bundles/angular2.dev.js',
-			'./node_modules/angular2/bundles/router.dev.js',
-			'./node_modules/angular2/bundles/http.dev.js',
-			'./node_modules/angular2/bundles/testing.dev.js'
-		];
-		this.testLibsOutputPath = `${this.testFilesOut}/lib`;
-		this.testLibsOutGlob = [`${this.testLibsOutputPath}/**/*.js`];
-		this.testLibsStyles = [
-			'./node_modules/jasmine-core/lib/jasmine-core/jasmine.css'
-		];
-		this.testLibsStylesOutputPath = `${this.testLibsOutputPath}/styles`;
-		this.testLibsStylesOutGlob = `${this.testLibsStylesOutputPath}/**/*.*`;
-	}
-}
-const settings = new Settings();
+const settings = require('./tasks/common/settings');
 
 const process = require('process');
 const gulp = require('gulp');
@@ -91,7 +15,7 @@ const sourcemaps = require('gulp-sourcemaps');
 const del = require('del');
 const tsProject = tsc.createProject('tsconfig.json', {typescript: require('typescript')});
 const merge = require('merge2');
-const browserSync = require('./tasks/delayed-browser-sync');
+const browserSync = require('./tasks/common/delayed-browser-sync');
 const superstatic = require('superstatic');
 const typedoc = require('gulp-typedoc');
 const historyApiFallback = require('connect-history-api-fallback');
